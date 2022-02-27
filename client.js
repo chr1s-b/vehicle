6a5f1c55aede8c84546e380adb2daaf6d91308c4
ninjutsufish <formaggio@hotmail.co.uk>
2022-02-27 04:58:04 +0000

    i am crying rn its 5am and i am so done its unbelievable

--- client.js
+++ client.js
@@ -110,6 +110,7 @@ function load(url, data, callback) {
 
     xhr.open('POST', url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Access-Control-Allow-Origin','https://vehiclegame.herokuapp.com')
     xhr.send(JSON.stringify(data));
 }
 function loadImageToPopUp(s){
